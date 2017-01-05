7373598b0485265841f2e76393b1953aea6f0f1d
baxiaxx <285558940@qq.com>
2017-01-05 18:48:07 +0800

    aabbcc

--- PodTestA112233/PodTestA112233.h
+++ PodTestA112233/PodTestA112233.h
@@ -6,14 +6,14 @@
 //  Copyright © 2017年 Moxian_X. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for PodTestA112233.
-FOUNDATION_EXPORT double PodTestA112233VersionNumber;
-
-//! Project version string for PodTestA112233.
-FOUNDATION_EXPORT const unsigned char PodTestA112233VersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PodTestA112233/PublicHeader.h>
+//#import <UIKit/UIKit.h>
+//
+////! Project version number for PodTestA112233.
+//FOUNDATION_EXPORT double PodTestA112233VersionNumber;
+//
+////! Project version string for PodTestA112233.
+//FOUNDATION_EXPORT const unsigned char PodTestA112233VersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <PodTestA112233/PublicHeader.h>
 
 
